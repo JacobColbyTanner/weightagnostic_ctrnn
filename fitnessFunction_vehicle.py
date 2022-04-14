8abf9359afde9b3940346b107d7056329c87f003
Jacob Colby Tanner <jacobcolbytanner@gmail.com>
2022-04-14 15:00:09 -0400

    initial commit

---
+++ fitnessFunction_vehicle.py
@@ -0,0 +1,76 @@
+from CTRNN import CTRNN
+import numpy as np
+import braitenberg as bv
+
+def fitnessFunction_vehicle(ctrnn_parameters, ctrnn_size, step_size):
+    duration = 50  
+    time = np.arange(0.0,duration,step_size)
+
+    ctrnn = CTRNN(size=ctrnn_size, step_size=step_size)
+
+    num_weights = ctrnn_size ** 2
+
+    new_weights = ctrnn_parameters[:num_weights]
+    
+    
+    #translate genome into ctrnn parameters
+    new = np.zeros(ctrnn_size**2)
+
+    #force same weights, positive and negative across the network 
+    for i in range(ctrnn_size**2):
+
+        if new_weights[i] > 1/3:
+            new[i] = 1
+        elif new_weights[i] < -1/3:
+            new[i] = -1
+        else:
+            new[i] = 0
+
+    ctrnn.weights = new.reshape((ctrnn_size, ctrnn.size))
+    ctrnn.taus = ctrnn_parameters[num_weights : (num_weights + ctrnn_size)]
+    ctrnn.biases = ctrnn_parameters[
+        (num_weights + ctrnn_size) : (num_weights + 2 * ctrnn_size)
+    ]
+
+
+    # Create the agent body
+    body = bv.Agent(ctrnn_size)
+
+    distance = 5
+    bearing = np.arange(0.0,2*np.pi,np.pi/4)
+
+    # Create stimuli in environment
+    steps = 0
+    finaldistance = 0
+    for angle in bearing:
+        food = bv.Food(distance,angle)
+
+        ctrnn_input = np.zeros(ctrnn_size)
+        # Use Euler method to update nervous system and agent body
+
+
+        
+
+        for t in time:
+            
+            # Set neuron input as the sensor activation levels
+            ctrnn_input[-2:] = body.sensor_state()
+            # Update the nervous system based on inputs
+
+            ctrnn.euler_step(ctrnn_input)
+
+            
+            # Update the body based on nervous system activity
+            states = ctrnn.outputs[0:2]
+
+            #print(states.shape)
+            motorneuron_outputs = states
+            body.step(food, motorneuron_outputs, step_size)
+            finaldistance += body.distance(food)
+            steps += 1
+
+    fitness = np.clip(1 - ((finaldistance/steps)/distance),0,1)
+
+    return fitness
+
+
