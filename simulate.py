55ba8f046ef77f7de47da7c9af8817a8e32b9c6d
Josh <joshnunley@gmail.com>
2022-04-19 15:28:09 -0400

    update simulate

--- simulate.py
+++ simulate.py
@@ -10,42 +10,10 @@ def simulate(ctrnn_parameters, ctrnn_size, step_size):
 
     ctrnn = CTRNN(size=ctrnn_size, step_size=step_size)
 
-    num_weights = ctrnn_size ** 2
-
-    new_weights = ctrnn_parameters[:num_weights]
-    # new = new_weights
-
-    # translate genome into ctrnn parameters
-    new = np.zeros(num_weights)
-
-    # force same weights, positive and negative across the network
-    for i in range(num_weights):
-        if new_weights[i] > 2 / 3:
-            new[i] = 1
-        elif new_weights[i] < 1 / 3:
-            new[i] = -1
-        else:
-            new[i] = 0
-
-
-    ctrnn.weights = new.reshape((ctrnn_size, ctrnn_size))
+    ctrnn.set_params(ctrnn_parameters)
 
     print("CTRNN Network weights")
     print(ctrnn.weights)
-
-
-    ctrnn.taus = ctrnn_parameters[num_weights : (num_weights + ctrnn_size)] + 0.0001
-    ctrnn.biases = 2 * (
-        ctrnn_parameters[(num_weights + ctrnn_size) : (num_weights + 2 * ctrnn_size)]
-        - 0.5
-    )
-
-    # ctrnn.weights = 2 * (new.reshape((ctrnn_size, ctrnn_size)) - 0.5)
-    # ctrnn.taus = ctrnn_parameters[num_weights : (num_weights + ctrnn_size)] + 0.0001
-    # ctrnn.biases = 2 * (
-    #    ctrnn_parameters[(num_weights + ctrnn_size) : (num_weights + 2 * ctrnn_size)]
-    #    - 0.5
-    # )
 
     #Run until transient dynamics are gone
     ctrnn_input = np.zeros(ctrnn_size)
