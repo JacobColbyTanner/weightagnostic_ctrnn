82950b1a627d8f9b65ec243cbe5f66d0588fa197
Jacob Tanner <jacobcolbytanner@gmail.com>
2022-04-20 17:07:22 -0400

    robustness addition

---
+++ fitnessFunction_vehicle_robust.py
@@ -0,0 +1,59 @@
+from CTRNN import CTRNN
+import numpy as np
+import braitenberg as bv
+
+
+def fitnessFunction_vehicle_robust(ctrnn_parameters, ctrnn_size, ctrnn_step_size, duration, distance, bv_step_size, transient_steps, robust_range, discrete=True):
+    
+    range_robust = np.arange(robust_range[0],robust_range[1],1)
+    fitness_all = np.zeros(len(range_robust))
+    for multiplier in range_robust:
+        time = np.arange(0.0, duration, bv_step_size)
+
+        ctrnn = CTRNN(size=ctrnn_size, step_size=ctrnn_step_size)
+        ctrnn.set_params(ctrnn_parameters, discrete=discrete)
+        ctrnn.weights = multiplier*ctrnn.weights
+
+        # Run to remove transient dynamics
+        ctrnn_input = np.zeros(ctrnn.size)
+
+        for i in range(transient_steps):
+            ctrnn.euler_step(ctrnn_input)
+
+        bearing = np.arange(0.0, 2 * np.pi, np.pi / 4)
+
+        # Create stimuli in environment
+        steps = 0
+        finaldistance = 0
+        for angle in bearing:
+            food = bv.Food(distance, angle)
+
+            # Create the agent body
+            body = bv.Agent()
+
+            for t in time:
+
+                # Set neuron input as the sensor activation levels
+                ctrnn_input[-2:] = body.sensor_state()
+                # Update the nervous system based on inputs
+
+                for i in range(int(bv_step_size/ctrnn.step_size)):
+                    ctrnn.euler_step(ctrnn_input)
+
+                # Update the body based on nervous system activity
+                states = ctrnn.outputs[0:2]
+
+                if np.isnan(np.sum(states)):
+                    return -100.0
+
+                # print(states.shape)
+                motorneuron_outputs = states
+                body.step(food, motorneuron_outputs, bv_step_size)
+                finaldistance += body.distance(food)
+                steps += 1
+
+        fitness_all[multiplier-1] = np.clip(1 - ((finaldistance / steps) / distance), 0, 1) - 0.01*np.sum(np.abs(ctrnn.weights))/(ctrnn.size**2)
+
+    fitness = np.mean(fitness_all)
+    return fitness
+
